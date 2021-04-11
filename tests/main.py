af1cc8885ad423770fe03f8e1dc044eea37a40f0
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-04-11 12:37:19 -0700

    begin main game

--- tests/main.py
+++ tests/main.py
@@ -1,83 +1,102 @@
 import pygame
 from pygame.locals import *
- 
-class App:
-    def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
- 
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._running = True
- 
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self._running = False
-    def on_loop(self):
-        pass
-    def on_render(self):
-        pass
-    def on_cleanup(self):
-        pygame.quit()
- 
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
- 
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
- 
-if __name__ == "__main__" :
-    theApp = App()
-    theApp.on_execute()
-import pygame
+import time
 
-#Define Grid Colors
+# Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GRAY = (200, 200, 200)
+ 
 
-# Set size of each grid cell
-WIDTH = 40
-HEIGHT = 40
-
-#Set margin between cells
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
+ 
+# This sets the margin between each cell
 MARGIN = 5
-
-# Create a 2 dimensional array
+ 
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
 grid = []
 for row in range(4):
+    # Add an empty array that will hold each cell
+    # in this row
     grid.append([])
     for column in range(4):
-        grid[row].append(0)
-
-# Set row 1, cell 2 to one
-grid[1][2] = 1
-
+        grid[row].append(0)  # Append a cell
+ 
 # Initialize pygame
 pygame.init()
-
+ 
 # Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [255, 255]
+WINDOW_SIZE = [640, 255]
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
-# Set title of screen
-pygame.display.set_caption('Fake Boggle')
+ # Text Variables
+text = 'this text is editable'
+font = pygame.font.SysFont(None, 48)
+img = font.render(text, True, RED)
 
+rect = img.get_rect()
+rect.topleft = (20, 20)
+# cursor = Rect(rect.topright, (3, rect.height))
+ 
+# Set title of screen
+pygame.display.set_caption("Fake Boggle")
+ 
 # Loop until the user clicks the close button.
 done = False
-
+ 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
+ 
 # -------- Main Program Loop -----------
 while not done:
-    for event in pygame.event.get()
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            # Change the x/y screen coordinates to grid coordinates
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            print("Click ", pos, "Grid coordinates: ", row, column)
+        elif event.type == KEYDOWN:
+            if event.key == K_BACKSPACE:
+                if len(text)>0:
+                    text = text[:-1]
+            else:
+                text += event.unicode
+            img = font.render(text, True, RED)
+            rect.size=img.get_size()
+            # cursor.topleft = rect.top
+    # Set the screen background
+    screen.fill(GRAY)
+    screen.blit(img, rect)
+    
+    # if time.time() % 1 > 0.5:
+    #     pygame.draw.rect(screen, RED, cursor)
+    # pygame.display.update()
 
-
+    # Draw the grid
+    for row in range(4):
+        for column in range(4):
+            color = WHITE
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+ 
+    # Limit to 60 frames per second
+    clock.tick(60)
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
